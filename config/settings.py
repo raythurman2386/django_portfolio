79706c31a83a476136dd810e1d87070a4169d26a
raythurman2386 <raymondthurman5@gmail.com>
2023-01-27 13:44:22 -0600

    getting ready for deployment

--- config/settings.py
+++ config/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+print(os.getenv('DB_NAME'))
 DATABASES = {
   'default': {
         'ENGINE': 'django.db.backends.postgresql',
