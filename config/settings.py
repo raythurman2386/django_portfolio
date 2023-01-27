15062e0e8f013847018a70b33bd8aee1d3cff145
raythurman2386 <raymondthurman5@gmail.com>
2023-01-27 14:55:03 -0600

    potentially this time

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_ORIGINS = ['http://*', 'https://*', 'newdjangoportfolio.herokuapp.com', 'http://newdjangoportfolio.herokuapp.com', 'https://newdjangoportfolio.herokuapp.com']
+ALLOWED_ORIGINS = ['http://*', 'https://*']
+ALLOWED_HOSTS = ['http://*', 'https://*']
 CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
 
 
