4d0e04303de3a4becd137b24c49f4734547c7b85
raythurman2386 <raymondthurman5@gmail.com>
2023-06-29 17:45:05 -0500

    final resume test suite for now :)

--- tests/resume_views_tests.py
+++ tests/resume_views_tests.py
@@ -1,13 +1,63 @@
-from django.http import FileResponse
-import os
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from resume.models import Summary, Projects, Employment, Education, Contact, Skill, Resume_PDF
+from resume.views import ResumePageView
 
 
-def download_resume(request):
-    resume_path = './media/Resume.pdf'
-    if os.path.exists(resume_path):
-        with open(resume_path, 'rb') as file:
-            response = FileResponse(file)
-            response['Content-Disposition'] = 'attachment; filename="RayThurmanResume.pdf"'
-            return response
-    else:
-        return HttpResponse('Resume not found.')
+class DownloadResumeViewTest(TestCase):
+    def test_download_resume(self):
+        response = self.client.get(reverse('download_resume'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.get('Content-Disposition'),
+            'attachment; filename="RayThurmanResume.pdf"'
+        )
+
+
+class ResumePageViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Summary.objects.create(
+            title='Test Resume Title',
+            description='Test Resume Description'
+        )
+        Projects.objects.create(
+            title='Test Project',
+            description='Test Description'
+        )
+        Employment.objects.create(
+            title='Test Title',
+            role='Test Role',
+            location='Test Location',
+            description='Test Description',
+            start_date='2023-01-01',
+            end_date='2023-01-01'
+        )
+        Education.objects.create(
+            title='Test Title',
+            description='Test Description',
+            start_date='2023-01-01',
+            end_date='2023-01-01'
+        )
+        Skill.objects.create(
+            skill='Test Skill'
+        )
+        Contact.objects.create(
+            address='Test Address',
+            email='test@email.com',
+            phone='1231231234'
+        )
+
+    def test_resume_page_view(self):
+        factory = RequestFactory()
+        request = factory.get(reverse('resume'))
+        view = ResumePageView()
+        view.setup(request)
+        context = view.get_context_data()
+
+        self.assertTrue('summary_data' in context)
+        self.assertTrue('projects_data' in context)
+        self.assertTrue('employment_data' in context)
+        self.assertTrue('education_data' in context)
+        self.assertTrue('skill_data' in context)
+        self.assertTrue('contact_data' in context)
