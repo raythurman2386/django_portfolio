9bcbf82abed1247cfaf65c2416082b129a72d9ac
raythurman2386 <raymondthurman5@gmail.com>
2023-07-13 15:55:49 -0500

    updating to env var tests passing ready for deploy

--- config/settings.py
+++ config/settings.py
@@ -18,9 +18,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT')
+SESSION_COOKIE_SECURE = os.environ.get('SECURE_SSL_REDIRECT')
+CSRF_COOKIE_SECURE = os.environ.get('SECURE_SSL_REDIRECT')
 
 ALLOWED_ORIGINS = ['http://*', 'https://*']
 ALLOWED_HOSTS = ['rthurman-portfolio-e6ccb38caa43.herokuapp.com',
