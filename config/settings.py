39ebb81bd8297334cbfc3c8825f10f2bd91bf94e
raythurman2386 <raymondthurman5@gmail.com>
2023-07-13 15:51:06 -0500

    set ssl to True for deployment for now

--- config/settings.py
+++ config/settings.py
@@ -18,9 +18,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_ORIGINS = ['http://*', 'https://*']
 ALLOWED_HOSTS = ['rthurman-portfolio-e6ccb38caa43.herokuapp.com',
