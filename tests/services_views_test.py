d9b8e863b6da6e480f03e2feac20f3ef74eae031
raythurman2386 <raymondthurman5@gmail.com>
2023-08-07 12:14:10 -0500

    corrected services test to match others

--- tests/services_views_test.py
+++ tests/services_views_test.py
@@ -1,36 +1,30 @@
-from django.test import TestCase, Client
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from services.models import Summary, Service, Contact
+from services.views import ServicePageView
 
 
 class ServicePageViewTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_service_page_view_context(self):
-        # Create sample data for Summary, Service, and Contact models
-        summary = Summary.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        Summary.objects.create(
             title="Test Summary", description="Test Description"
         )
-        service = Service.objects.create(
+        Service.objects.create(
             title="Test Service", description="Test Description"
         )
-        contact = Contact.objects.create(
+        Contact.objects.create(
             address="Test Address", email="test@example.com", phone="1234567890"
         )
 
-        # Simulate a request to the ServicePageView
-        response = self.client.get(reverse("services"))
-
-        # Check if the response is successful (status code 200)
-        self.assertEqual(response.status_code, 200)
+    def test_service_page_view(self):
+        factory = RequestFactory()
+        request = factory.get(reverse("services"))
+        view = ServicePageView()
+        view.setup(request)
+        context = view.get_context_data()
 
         # Check if the context data is correctly populated
-        self.assertEqual(len(response.context["summary_data"]), 1)
-        self.assertEqual(len(response.context["service_data"]), 1)
-        self.assertEqual(len(response.context["contact_data"]), 1)
-
-        # Check the values of specific objects
-        self.assertEqual(response.context["summary_data"][0].title, "Test Summary")
-        self.assertEqual(response.context["service_data"][0].title, "Test Service")
-        self.assertEqual(response.context["contact_data"][0].email, "test@example.com")
+        self.assertTrue("summary_data" in context)
+        self.assertTrue("service_data" in context)
+        self.assertTrue("contact_data" in context)
